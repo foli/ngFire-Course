519d1d1ffbb518614ef87b84c2975b299abdbd47
flavio Oliveira <flavioho@me.com>
2018-03-15 11:42:33 +0100

    Add userProfile + getUser function

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { UserService } from "../user.service";
+import { User } from "../user.model";
 
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  selector: "app-user-detail",
+  templateUrl: "./user-detail.component.html",
+  styleUrls: ["./user-detail.component.css"]
 })
 export class UserDetailComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    this.getUser()
+  }
+
+  getUser(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.userService.getUser(id).subscribe(user => this.user = user)
   }
 
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -11,12 +11,18 @@ import { User } from "./user.model";
 @Injectable()
 export class UserService {
   userCollection: AngularFirestoreCollection<User>;
+  userDoc: AngularFirestoreDocument<User>
 
   constructor(private afs: AngularFirestore) {}
 
   getUsers() {
     this.userCollection = this.afs.collection('users')
     return this.userCollection.valueChanges()
+  }
+  
+  getUser(id: string) {
+    this.userDoc = this.afs.doc<User>(`users/${id}`)
+    return this.userDoc.valueChanges()
   }
   
 }
