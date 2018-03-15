1d12274ba644e520c2506d36e05ce79beb849bf3
flavio Oliveira <flavioho@me.com>
2018-03-15 13:24:48 +0100

    Add post dashboard + create function

--- src/app/post/post-dashboard/post-dashboard.component.ts
+++ src/app/post/post-dashboard/post-dashboard.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+
+import { AuthService } from "../../core/auth.service";
+import { PostService } from "../post.service";
+import { Post } from "../post.model";
 
 @Component({
-  selector: 'app-post-dashboard',
-  templateUrl: './post-dashboard.component.html',
-  styleUrls: ['./post-dashboard.component.css']
+  selector: "app-post-dashboard",
+  templateUrl: "./post-dashboard.component.html",
+  styleUrls: ["./post-dashboard.component.css"]
 })
 export class PostDashboardComponent implements OnInit {
+  postForm: FormGroup;
 
-  constructor() { }
+  imageURL: string;
+
+  constructor(
+    private postService: PostService,
+    private auth: AuthService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.createForm();
   }
 
+  createForm() {
+    this.postForm = this.fb.group({
+      title: [""],
+      content: [""],
+      draft: false
+    });
+  }
+
+  savePost() {
+    const formData: Post = {
+      author: this.auth.authState.displayName || this.auth.authState.email,
+      title: this.postForm.get("title").value,
+      image: this.imageURL || null,
+      content: this.postForm.get("content").value,
+      draft: this.postForm.get("draft").value || false,
+      published: new Date(),
+      claps: 0
+    };
+    if (!this.postForm.untouched) {
+      this.postService.create(formData);
+      this.postForm.reset();
+    }
+  }
 }
--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore'
+
+import { Post } from './post.model'
 
 @Injectable()
 export class PostService {
+  postsCollection: AngularFirestoreCollection<Post>
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+    this.postsCollection = this.afs.collection('posts', ref =>
+      ref.orderBy('claps', 'desc').limit(10)
+    )
+  }
+
+  create(data: Post) {
+    this.postsCollection.add(data)
+  }
+
 
 }
