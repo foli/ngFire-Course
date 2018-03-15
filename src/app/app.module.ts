892166207d19faa21a15ae6e582a73095a87f64e
flavio Oliveira <flavioho@me.com>
2018-03-15 23:01:39 +0100

    Add service worker + firebase configs/deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 import { environment } from "../environments/environment";
 
 import { AngularFireModule } from "angularfire2";
@@ -21,6 +23,7 @@ import { RoutingGuard } from './routing.guard';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     AngularFireModule.initializeApp(environment.firebase),
     MaterialModule,
     CoreModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsS7MpG6--ARrlc5mYbYpm2bWcNYCaQaY",
+    authDomain: "foli-sandbox.firebaseapp.com",
+    databaseURL: "https://foli-sandbox.firebaseio.com",
+    projectId: "foli-sandbox",
+    storageBucket: "foli-sandbox.appspot.com",
+    messagingSenderId: "58657445594"
+  }
 };
