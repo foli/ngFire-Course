d3457d2fc066a630fbcc6e3c05d9adde95682d1c
Flavio Oliveira <flavio@foli.dev>
2021-05-22 14:15:01 +0200

    (app) add readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBsS7MpG6--ARrlc5mYbYpm2bWcNYCaQaY",
-    authDomain: "foli-sandbox.firebaseapp.com",
-    databaseURL: "https://foli-sandbox.firebaseio.com",
-    projectId: "foli-sandbox",
-    storageBucket: "foli-sandbox.appspot.com",
-    messagingSenderId: "58657445594"
-  }
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBsS7MpG6--ARrlc5mYbYpm2bWcNYCaQaY",
+        authDomain: "foli-sandbox.firebaseapp.com",
+        databaseURL: "https://foli-sandbox.firebaseio.com",
+        projectId: "foli-sandbox",
+        storageBucket: "foli-sandbox.appspot.com",
+        messagingSenderId: "58657445594"
+    }
 };
